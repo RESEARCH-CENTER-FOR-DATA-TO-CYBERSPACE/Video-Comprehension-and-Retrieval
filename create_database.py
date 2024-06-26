079604d8146bc8b20e849db89d695d1735c10680
Xiaorui Jiang <76674402+xiaorui-jiang@users.noreply.github.com>
2024-06-26 13:24:44 +0800

    update main.py & creat_database.py

--- create_database.py
+++ create_database.py
@@ -1,63 +1,72 @@
 import pandas as pd
 from elasticsearch import Elasticsearch
 import argparse
-
+from pathlib import Path
 parser = argparse.ArgumentParser(
                     prog='createDatabase',
                     description='create the elasticsearch database and add the documents along with their embeddings')
 
 parser.add_argument('-d', '--data_path', help='enter the path to the .csv file that contains the embedding vectors')
-parser.add_argument('--host', default='127.0.0.1', help='enter the host which the elasticsearch server listens to')
 parser.add_argument('-p', '--port', default=9200, help='enter the port which the elasticsearch server listens to')
 
 # Parse the arguments
 args = parser.parse_args()
-
-# args.data_path = './6a215328-9bf3-4525-bb3c-124dd4efca66/video_embedding.json'
+import os
 
 # Establish connection with the elasticsearch server
-es = Elasticsearch([{'host': args.host, 'port':args.port, 'scheme':'http'}])
-
+es = Elasticsearch([{'host': '211.86.152.66', 'port': 9200, 'scheme': 'http'}])
 # Delete the Database if it exists
-es.options(ignore_status=[400,404]).indices.delete(index='video_cases')
+es.options(ignore_status=[400, 404]).indices.delete(index='video_cases')
 
 # Specify the mapping, which will describe the structure of each document
 mappings = {
-	"properties":{
-		"time":{
-            "type": "text", 
+	"properties": {
+		"video_id": {
+			"type": "text",
 		},
-		"image_text":{
-				"type":"text"
+		"time": {
+			"type": "text",
 		},
-		"image_emb":{
-				"type": "dense_vector",
-				"dims": 768
+		"image_text": {
+			"type": "text"
 		},
-		"audio_text":{
-				"type":"text"
+		"image_emb": {
+			"type": "dense_vector",
+			"dims": 768
 		},
-		"audio_emb":{
-				"type": "dense_vector",
-				"dims": 768
-		}    
-    }
+		"audio_text": {
+			"type": "text"
+		},
+		"audio_emb": {
+			"type": "dense_vector",
+			"dims": 768
+		}
+	}
 }
 
 # Create the index
 result = es.indices.create(index="video_cases", mappings=mappings)
 
-# Access the data that are going to be uploaded on the server
-embeddings_df = pd.read_json(str(args.data_path))
+items = Path("./hash_folders").iterdir()
+folder_names = [item.name for item in items if item.is_dir()]
+for folder_name in folder_names:
 
-# Iterate over the rows of the csv and add each document on the database
-for index, row in embeddings_df.iterrows():
-	doc = {
-		'time': str(row['time']),
-		'image_text' : str(row['image_text']),
-		"image_emb": row['image_emb'],
-		'audio_text': str(row['audio_text']),
-		"audio_emb": row['audio_emb'],
-	}
+	args.data_path = './hash_folders/' + folder_name + '/video_embedding.json'
 
-	res = es.index(index="video_cases", id=index, document=doc)
+	# Access the data that are going to be uploaded on the server
+	embeddings_df = pd.read_json(str(args.data_path))
+
+	# Iterate over the rows of the csv and add each document on the database
+	for index, row in embeddings_df.iterrows():
+		doc = {
+			'video_id': str(row['video_id']),
+			'time': str(row['time']),
+			'image_text' : str(row['image_text']),
+			"image_emb": row['image_emb'],
+			'audio_text': str(row['audio_text']),
+			"audio_emb": row['audio_emb'],
+		}
+		# 生成一个唯一的文档 ID
+		doc_id = f"{row['video_id']}_{row['time']}"
+		# 使用文档 ID 添加文档到索引中，确保不会覆盖
+		res = es.index(index="video_cases", id=doc_id, document=doc)
--- main.py
+++ main.py
@@ -19,10 +19,7 @@ from openai import OpenAI
 from tqdm import tqdm
 from openai import AzureOpenAI
 
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s - %(message)s')
-
-
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 def parse_time(time_str):
@@ -41,7 +38,9 @@ def format_time(td):
     milliseconds = td.microseconds // 1000
     return f"{hours:02}:{minutes:02}:{seconds:02},{milliseconds:03}"
 
-def read_and_extract_srt(file_path, start_time_str='00:00:00,000', end_time_str='100:00:00,000', interval_seconds=60, padding_seconds=5):
+
+def read_and_extract_srt(file_path, start_time_str='00:00:00,000', end_time_str='100:00:00,000', interval_seconds=60,
+                         padding_seconds=5):
     """Reads SRT content from file and extracts subtitles within specified time range split by intervals with padding."""
     start_time = parse_time(start_time_str)
     end_time = parse_time(end_time_str)
@@ -93,17 +92,18 @@ def read_and_extract_srt(file_path, start_time_str='00:00:00,000', end_time_str=
 
     return subtitles
 
+
 def save_to_json(data, output_path):
     """Saves subtitle data to a JSON file."""
     with open(output_path, 'w', encoding='utf-8') as json_file:
         json.dump(data, json_file, ensure_ascii=False, indent=4)
 
 
-def encode_text(image2text_path, audio2text_path, output_path, mdoel_name='uer/sbert-base-chinese-nli'): 
+def encode_text(video_id, image2text_path, audio2text_path, output_path, mdoel_name='uer/sbert-base-chinese-nli'):
     # 加载预训练的模型和分词器
     model = SentenceTransformer(mdoel_name, trust_remote_code=True)
     model.max_seq_length = 512
-    df = pd.DataFrame(columns=['time', 'image_text', 'image_emb', 'audio_text', 'audio_emb'])
+    df = pd.DataFrame(columns=['video_id', 'time', 'image_text', 'image_emb', 'audio_text', 'audio_emb'])
     data2 = None
     if os.path.exists(audio2text_path):
         with open(audio2text_path, 'r', encoding='utf-8') as f2:
@@ -116,10 +116,14 @@ def encode_text(image2text_path, audio2text_path, output_path, mdoel_name='uer/s
             if data2:
                 text2 = data2[time].replace(' ', '')
                 embedding2 = model.encode(text2)
-                df = pd.concat([df, pd.DataFrame({'time': time, 'image_text': text1, 'image_emb': [embedding1], 'audio_text': text2, 'audio_emb': [embedding2]})], ignore_index=True)
+                df = pd.concat([df, pd.DataFrame(
+                    {'video_id': video_id, 'time': time, 'image_text': text1, 'image_emb': [embedding1], 'audio_text': text2,
+                     'audio_emb': [embedding2]})], ignore_index=True)
             else:
-                df = pd.concat([df, pd.DataFrame({'time': time, 'image_text': text1, 'image_emb': [embedding1]})], ignore_index=True)
+                df = pd.concat([df, pd.DataFrame({'video_id': video_id, 'time': time, 'image_text': text1, 'image_emb': [embedding1]})], ignore_index=True)
+
     df.to_json(output_path, index=False)
+
 
 def video_downsample(video_name, output_path, timestamps=None, time_rate=1, time_delta=60):
     import cv2
@@ -130,11 +134,11 @@ def video_downsample(video_name, output_path, timestamps=None, time_rate=1, time
     cap = cv2.VideoCapture(video_name)
     fps = cap.get(5)  # 获取视频帧率
     total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))  # 获取总帧数
-    video_length_seconds = total_frames / fps # 计算视频总长度（秒）
+    video_length_seconds = total_frames / fps  # 计算视频总长度（秒）
     if not timestamps:
         timestamps = [format_time(timedelta(seconds=x)) for x in range(0, int(video_length_seconds), time_delta)]
     timestamps.append(format_time(timedelta(seconds=video_length_seconds)))
-    
+
     # 对于每对时间点
     for i in range(len(timestamps) - 1):
         start_td = parse_time(timestamps[i])
@@ -144,7 +148,7 @@ def video_downsample(video_name, output_path, timestamps=None, time_rate=1, time
         clip_path = os.path.join(output_path, f'{format_time(start_td)}')
         os.mkdir(clip_path)
         while current_td < end_td:
-            
+
             cap.set(cv2.CAP_PROP_POS_MSEC, min(current_td.total_seconds(), video_length_seconds) * 1000)  # 设置视频时间
             ret, frame = cap.read()  # 读取帧
             if not ret:
@@ -177,43 +181,46 @@ def video_downsample(video_name, output_path, timestamps=None, time_rate=1, time
 
 # Function to encode the image
 def encode_image(image_path):
-  with open(image_path, "rb") as image_file:
-    return base64.b64encode(image_file.read()).decode('utf-8')
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+
 
 def convert_img2text_raw(image_path):
-
     images = [os.path.join(image_path, file) for file in os.listdir(image_path)]
     for image in images:
         # Getting the base64 string
         base64_image = encode_image(image)
 
         response = client.chat.completions.create(
-        model=gpt_model,
-        messages=[
-            {
-            "role": "user",
-            "content": [
-                {"type": "text", "text": "What’s in this image? Make sure you use the same language as the image."},
+            model=gpt_model,
+            messages=[
                 {
-                "type": "image_url",
-                "image_url": {
-                    "url": f"data:image/jpeg;base64,{base64_image}",
-                },
-                },
+                    "role": "user",
+                    "content": [
+                        {"type": "text",
+                         "text": "What’s in this image? Make sure you use the same language as the image."},
+                        {
+                            "type": "image_url",
+                            "image_url": {
+                                "url": f"data:image/jpeg;base64,{base64_image}",
+                            },
+                        },
+                    ],
+                }
             ],
-            }
-        ],
-        max_tokens=300,
+            max_tokens=300,
         )
 
         print(response.choices[0])
         break
+
 
 # 将时间转换为秒
 def time_to_seconds(t):
     h, m, s = t.split(':')
     s, ms = s.split(',')
     return int(h) * 3600 + int(m) * 60 + int(s) + int(ms) / 1000
+
 
 def convert_img2text(clips_path):
     contents = {}
@@ -224,27 +231,20 @@ def convert_img2text(clips_path):
         messages = [
             {"role": "system", "content": "You are a helpful assistant designed to output JSON."},
             {
-            "role": "user",
-            "content": [
-                {"type": "text", "text": "下面一组图片是从一个视频中采样得到的，请根据这些帧总结视频内容。注意输出json中只需要包含'content'字段。"},
-            ],
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": "下面一组图片是从一个视频中采样得到的，请根据这些帧总结视频内容。注意输出json中只需要包含'content'字段。"},
+                ],
             }
         ]
         for image in images:
             # Getting the base64 string
             base64_image = encode_image(image)
-            messages[1]['content'].append({"type": "image_url", "image_url": { "url": f"data:image/jpeg;base64,{base64_image}", "detail": "low"}})
-        # messages = [
-        #     {
-        #         "role": "user",
-        #         "content": [
-        #             "These are frames from a video that I want to upload. Generate a compelling description that I can upload along with the video.",
-        #             *map(lambda x: {"image": encode_image(os.path.join(image_path, x)), "resize": 768}, images[::]),
-        #         ],
-        #     },
-        # ]
+            messages[1]['content'].append(
+                {"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{base64_image}", "detail": "low"}})
+
         response = client.chat.completions.create(
-            response_format={"type": "json_object" },
+            response_format={"type": "json_object"},
             model=gpt_model,
             messages=messages,
             temperature=0,
@@ -255,17 +255,17 @@ def convert_img2text(clips_path):
         except:
             logging.error(f'{clip} response {response.choices[0].message.content} parse error!')
             contents[f'{clip}'] = response.choices[0].message.content
-        
-    return contents
-  
-def speech2text(video, mdoel_path):
 
+    return contents
+
+
+def speech2text(video, mdoel_path, filename):
     SetLogLevel(-1)
     model = Model(model_path=mdoel_path)
     logging.info("sp2t setup")
 
-    video_id = str(uuid.uuid4())
-    os.makedirs(f"{video_id}")
+
+    video_id = './hash_folders/' + filename
     os.makedirs(f"{video_id}/tmp")
 
     audio = f"{video_id}/audio.wav"
@@ -285,12 +285,11 @@ def speech2text(video, mdoel_path):
 
 
 if __name__ == "__main__":
-
+    os.environ["CUDA_VISIBLE_DEVICES"] = "1"
     parser = argparse.ArgumentParser()
-    parser.add_argument("-v", "--video", type=str, required=False, default="")
+    parser.add_argument("-v", "--video", type=str, required=False, default="./videos")
     args = parser.parse_args()
 
-    video_path = args.video if args.video else './video.mp4'
 
     gpt_model = 'gpt-4o'
     client = OpenAI(
@@ -298,39 +297,49 @@ if __name__ == "__main__":
     )
     speech_recognition_model = './model/vosk-model-cn-0.22'
     embedding_model = './model/uer_sbert-base-chinese-nli'
-            
-    
-    logging.info('开始视频处理')
-    video_id = speech2text(video_path, speech_recognition_model) # 'd7b24747-fade-4765-91e4-40ce04d7865d'
-    time_rate = 5 # 每隔timeRate(s)截取一帧
-    time_delta = 30 # 每个切片time_delta(s)
-    
-    # 文件路径
-    speech_path = f"{video_id}/audio.wav"
-    srt_path = f'{video_id}/captions.srt'
-    audio2text_path = f'{video_id}/video_text.json'
-    image2text_path = f'{video_id}/image_text.json'
-    embedding_path = f'{video_id}/video_embedding.json'
-    capture_img_path = f"./{video_id}/capture_image"
 
-    # 读取、切片和保存SRT文件
-    subtitles = read_and_extract_srt(srt_path, interval_seconds=time_delta)
-    save_to_json(subtitles, audio2text_path)
+    # 递归遍历文件夹及其子文件夹
+    for root, dirs, files in os.walk(args.video):
+        for filename in files:
+            video_path = os.path.join(root, filename)
+            logging.info('开始视频处理')
+            tmp_id = './hash_folders/' + filename
+            try:
+                os.makedirs(tmp_id)
+            except FileExistsError:
+                logging.info('已处理该视频文件，跳过')
+                continue
+            video_id = speech2text(video_path, speech_recognition_model, filename)  # 'd7b24747-fade-4765-91e4-40ce04d7865d'
+            time_rate = 5  # 每隔timeRate(s)截取一帧
+            time_delta = 30  # 每个切片time_delta(s)
 
-    logging.info(f'提取音频位置为：{speech_path}')
-    logging.info(f'语音识别结果：{audio2text_path}')
-    timestamps = None
-    if os.path.exists(audio2text_path):
-        with open(audio2text_path, 'r') as file:
-            timestamps = list(json.load(file).keys())
+            # 文件路径
+            speech_path = f"{video_id}/audio.wav"
+            srt_path = f'{video_id}/captions.srt'
+            audio2text_path = f'{video_id}/video_text.json'
+            image2text_path = f'{video_id}/image_text.json'
+            embedding_path = f'{video_id}/video_embedding.json'
+            capture_img_path = f"./{video_id}/capture_image"
 
-    # 视频帧采样
-    video_downsample(video_path, capture_img_path, timestamps, time_rate=time_rate, time_delta=time_delta)
+            # 读取、切片和保存SRT文件
+            subtitles = read_and_extract_srt(srt_path, interval_seconds=time_delta)
+            save_to_json(subtitles, audio2text_path)
 
-    # image to text using GPT
-    img_contents = convert_img2text(capture_img_path)
-    save_to_json(img_contents, image2text_path)
+            logging.info(f'提取音频位置为：{speech_path}')
+            logging.info(f'语音识别结果：{audio2text_path}')
+            timestamps = None
 
-    # encode text using sbert
-    encode_text(image2text_path, audio2text_path, embedding_path, embedding_model)
+            if os.path.exists(audio2text_path):
+                with open(audio2text_path, 'r') as file:
+                    timestamps = list(json.load(file).keys())
+
+            # 视频帧采样
+            video_downsample(video_path, capture_img_path, timestamps, time_rate=time_rate, time_delta=time_delta)
+
+            # image to text using GPT
+            img_contents = convert_img2text(capture_img_path)
+            save_to_json(img_contents, image2text_path)
+
+            # encode text using sbert
+            encode_text(video_id, image2text_path, audio2text_path, embedding_path, embedding_model)
 
